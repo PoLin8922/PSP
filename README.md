a3a119a02b2061fd5d6371e9c0e9f5025be0d81a
tingweiou <kklb1716@gmail.com>
2024-04-28 11:33:46 +0800

    capture debug and optimized ok

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
-realsense:
+## commom operation
+- launch the realsense:
 ```
 roslaunch realsense2_camera demo_pointcloud.launch
+```
+
+- launch controller ( whole pkg)
+```
+roslaunch controller controller.py
+```
+
+- rosrun individual pkg(exampel rosrun capture)
+```
+rosrun vision_capture2 vision_capture.py
 ```
 
 srv:
 ```
 rosservice call /vision_capture "scan: true"
+```
+
+## system installation
+- upgrade cmake -> https://blog.csdn.net/qq_36852840/article/details/128338724
+- remember to reinstall ros after remove origin cmake
+- libmobus
+```
+sudo apt-get install libmodbus-dev
+sudo apt-get install modbus*
+```
+- Open3d
+please follow the offical instruction(c++) -> https://www.open3d.org/docs/release/compilation.html
+```
+sudo pip install open3d
 ```
--- src/vision_capture2/src/vision_capture.py
+++ src/vision_capture2/src/vision_capture.py
@@ -10,11 +10,6 @@ import sensor_msgs.point_cloud2 as pc2
 import open3d as o3d
 from vision_capture2.srv import VisionCapture, VisionCaptureResponse
 
-global centroid
-
-cloud_data = None
-centroid = None
-
 def cloud_callback(msg):
     global cloud_data
     cloud_data = msg
@@ -24,7 +19,7 @@ def LOGGING(state):
     if homeDir is None:
         sys.stderr.write("Failed to get the home directory.\n")
 
-    file_loc = '/home/honglang/PSP/logfile/logging_file.txt'
+    file_loc = 'logfile/logging_file.txt'
 
     with open(file_loc, 'a') as file:
         if file.tell() != 0:
@@ -45,47 +40,85 @@ def save_point_cloud_as_pcd():
         )
 
         cropped_pcd = pcd.crop(bounding_box)
+        
+        filtered_pcd = average_z_filter_for_x_direction(cropped_pcd)
 
-        #centroid_pcd = center_point_cloud(downsampled_pcd)
+        open("files/point_cloud.pcd", "w").close()
 
-        # find the max_x & min_x
-        max_x = float('-inf')
-        min_x = float('inf')
-        for point in cropped_pcd.points:
-            x = point[0]
-            if x > max_x:
-                max_x = x
-            if x < min_x:
-                min_x = x
+        o3d.io.write_point_cloud("files/point_cloud.pcd", filtered_pcd)
 
-        x_resolution = 0.005 # resolution = 1cm
-        current_x = min_x
 
-        # modified ! mean
+def get_boundary( pcd ):
+    points = np.asarray(pcd.points)
 
-        while current_x < max_x:
-            loop_min_x = 100
-            for tmp_points in cropped_pcd.points:
-                if tmp_points[0] < current_x + x_resolution and tmp_points[0] >= current_x:
-                    if tmp_points[2] < loop_min_x :
-                        loop_min_x = tmp_points[2]
-            for tmp_points in cropped_pcd.points:
-                if tmp_points[0] < current_x + x_resolution and tmp_points[0] >= current_x:
-                    tmp_points[2] = loop_min_x 
+    min_x = np.min(points[:, 0])
+    max_x = np.max(points[:, 0])
+    min_y = np.min(points[:, 1])
+    max_y = np.max(points[:, 1])
 
-            current_x = current_x + x_resolution
+    point_cloud = o3d.geometry.PointCloud()
+    boundary_cloud = np.array([[min_x, min_y, 30], [min_x, max_y, 30], [max_x, max_y, 30], [max_x, min_y, 30], [min_x, min_y, 30]])
+    point_cloud.points = o3d.utility.Vector3dVector(boundary_cloud)
+    
+    #for p in point_cloud.points:
+    #    print("x:", p[0], "y:", p[1], "z:", p[2])
 
-        # o3d.io.write_point_cloud( homeDir + "/PSP/files/point_cloud.pcd", centroid_pcd )
-        o3d.io.write_point_cloud("/home/honglang/PSP/files/point_cloud.pcd", cropped_pcd)
-        # for point in cropped_pcd.points:
-        #     print("x:", point[0], "y:", point[1], "z:", point[2])
+    o3d.io.write_point_cloud("files/boundary_cloud.pcd", point_cloud)
+
+def average_z_filter_for_x_direction(cropped_pcd):
+    filtered_pcd = o3d.geometry.PointCloud()
+        
+    # find the max_x & min_x
+    max_x = float('-inf')
+    min_x = float('inf')
+    for point in cropped_pcd.points:
+        x = point[0]
+        if x > max_x:
+            max_x = x
+        if x < min_x:
+            min_x = x
+
+    x_resolution = 0.005 # resolution = 1cm
+    current_x = min_x
+
+    while current_x < max_x:
+        loop_avg_z = 0
+        loop_sum_z = 0
+        count = 0
+        for tmp_points in cropped_pcd.points:
+            if tmp_points[0] < current_x + x_resolution and tmp_points[0] >= current_x:
+                loop_sum_z += tmp_points[2]
+                count += 1
+
+        if count > 0:
+            loop_avg_z = loop_sum_z / count
+
+        for tmp_points in cropped_pcd.points:
+            if tmp_points[0] < current_x + x_resolution and tmp_points[0] >= current_x:
+                tmp_points[2] = loop_avg_z
+                
+                filtered_pcd.points.append(tmp_points[:3])  # Only keep the x, y, z coordinates
+
+        current_x = current_x + x_resolution
+
+    return filtered_pcd
 
 def capture(req):
-    # print("fuck")
-    save_point_cloud_as_pcd()
+    global cloud_data
+    cloud_data = None
+
     if req.scan == True:
+        # if msg(subscriber) is not None
+        while cloud_data is None:
+            rospy.Subscriber("/camera/depth/color/points", PointCloud2, cloud_callback)
+        
         save_point_cloud_as_pcd()
-        cropped_pcd = o3d.io.read_point_cloud("/home/honglang/PSP/files/point_cloud.pcd")
+        
+        cropped_pcd = o3d.io.read_point_cloud("files/point_cloud.pcd")
+
+        get_boundary( cropped_pcd )
+
+        boundary_cloud = o3d.io.read_point_cloud("files/boundary_cloud.pcd")
 
         # visualize
         axes = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1, origin=np.zeros(3))
@@ -93,15 +126,11 @@ def capture(req):
         cropped_pcd.colors = o3d.utility.Vector3dVector(blue_color)
         scene = [cropped_pcd, axes]
         o3d.visualization.draw_geometries(scene, window_name="Cropped Point Cloud with XYZ Axes", width=800, height=600)
+        o3d.visualization.draw_geometries([boundary_cloud], window_name="Cropped Point Cloud with XYZ Axes", width=800, height=600)
 
         return VisionCaptureResponse(True)
 
-def ros_server():
-    
+if __name__ == '__main__':
     rospy.init_node('vision_capture')
-    rospy.Subscriber("/camera/depth/color/points", PointCloud2, cloud_callback)
     s = rospy.Service('/vision_capture', VisionCapture, capture)
     rospy.spin()
-
-if __name__ == '__main__':
-    ros_server()
